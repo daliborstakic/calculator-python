a086248800b3ba272b6583e915005a2d55142e8d
daliborstakic <stakicdalibor64@gmail.com>
2020-09-11 17:25:37 +0200

    added number entry fields

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 """ Importing tkinter """
 import tkinter as tk
-from unicodedata import name
+from tkinter import Entry, Label
 
 class CalcApp:
     """ Calculator app """
@@ -8,6 +8,17 @@ class CalcApp:
         """ Init class """
         self.root = root
         root.title("Calculator")
+
+        # Number entry fields
+        self.first_label = Label(root, text="First number:").grid(row=0, column=0, padx=5, pady=5)
+        self.first_entry = Entry(root)
+
+        self.first_entry.grid(row=0, column=1, pady=5, padx=5)
+
+        self.second_label = Label(root, text="Second number:").grid(row=1, column=0, padx=5, pady=5)
+        self.second_entry = Entry(root)
+
+        self.second_entry.grid(row=1, column=1, pady=5, padx=5)
 
 # Creating the app
 root = tk.Tk()
