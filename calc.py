ed1371a8affa241fb0c28dadaae8a96a54a96718
daliborstakic <stakicdalibor64@gmail.com>
2020-09-12 09:48:41 +0200

    added buttons|

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 """ Importing tkinter """
 import tkinter as tk
-from tkinter import Entry, Label
+from tkinter import Button, Entry, Label, StringVar
 
 class CalcApp:
     """ Calculator app """
@@ -9,16 +9,39 @@ class CalcApp:
         self.root = root
         root.title("Calculator")
 
-        # Number entry fields
-        self.first_label = Label(root, text="First number:").grid(row=0, column=0, padx=5, pady=5)
-        self.first_entry = Entry(root)
+        # Equation
+        equation = StringVar()
 
-        self.first_entry.grid(row=0, column=1, pady=5, padx=5)
+        # Equation entry fields
+        self.first_entry = Entry(root, textvariable=equation)
+        self.first_entry.grid(columnspan=4, ipadx=70, pady=(5, 0))
 
-        self.second_label = Label(root, text="Second number:").grid(row=1, column=0, padx=5, pady=5)
-        self.second_entry = Entry(root)
+        # Setting initial value
+        equation.set("Enter your expression")
 
-        self.second_entry.grid(row=1, column=1, pady=5, padx=5)
+        # Button first row
+        self.button1 = Button(root, text=" 1 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=1, column=0)
+        self.button2 = Button(root, text=" 2 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=1, column=1)
+        self.button3 = Button(root, text=" 3 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=1, column=2)
+        self.add_button = Button(root, text=" + ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=1, column=3)
+
+        # Second row
+        self.button4 = Button(root, text=" 4 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=2, column=0)
+        self.button5 = Button(root, text=" 5 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=2, column=1)
+        self.button6 = Button(root, text=" 6 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=2, column=2)
+        self.subtract_button = Button(root, text=" - ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=2, column=3)
+
+        # Third row
+        self.button7 = Button(root, text=" 7 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=3, column=0)
+        self.button8 = Button(root, text=" 8 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=3, column=1)
+        self.button9 = Button(root, text=" 9 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=3, column=2)
+        self.multiple_button = Button(root, text=" + ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=3, column=3)
+
+        # Fourth row
+        self.button0 = Button(root, text=" 0 ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=4, column=0, pady=(0,5))
+        self.clear_button = Button(root, text="Clear", width=7, height=1, bg="#D4C13D", fg="white").grid(row=4, column=1, pady=(0,5))
+        self.equal_button = Button(root, text=" = ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=4, column=2, pady=(0,5))
+        self.divide_button = Button(root, text=" / ", width=7, height=1, bg="#D4C13D", fg="white").grid(row=4, column=3, pady=(0,5))
 
 # Creating the app
 root = tk.Tk()
